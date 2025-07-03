aabcb0e46fb8374ec4805d0aaa8ad22377621356
baseplate-admin <61817579+baseplate-admin@users.noreply.github.com>
2025-07-03 19:12:53 +0600

    add

--- webpack/webpack.config.js
+++ webpack/webpack.config.js
@@ -1,11 +1,17 @@
 const path = require('path');
 const nodeExternals = require('webpack-node-externals');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = {
   entry: './src/main.ts',
   target: 'node',
   mode: 'production',
-  externals: [nodeExternals()],
+
+  externals: [
+    nodeExternals({
+      allowlist: [/\.node$/], // Bundle .node files instead of externalizing
+    }),
+  ],
 
   module: {
     rules: [
@@ -16,15 +22,21 @@ module.exports = {
           loader: 'swc-loader',
         },
       },
+      {
+        test: /\.node$/,
+        use: 'node-loader',
+      },
     ],
   },
 
   resolve: {
-    extensions: ['.ts', '.js'],
+    extensions: ['.ts', '.js', '.node'],
   },
 
   output: {
     path: path.resolve(__dirname, '..', 'dist'),
     filename: 'main.js',
   },
+
+  plugins: [new CleanWebpackPlugin()],
 };
